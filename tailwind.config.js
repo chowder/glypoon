9616ab0709a2f0fae0d5323e13224617888e537c
chowder <andrewcxw@gmail.com>
2021-04-05 01:37:32 +0100

    Add some feedback for an incorrect answer
    Fixed an issue with hiding the glypoon letters

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-1deg)' },
+          '50%': { transform: 'rotate(1deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 200ms ease-in-out',
+      }
+    },
   },
   variants: {
     extend: {},
