0adfae755c515407dd1fcf3963f5f9108fd21d14
chowder <andrewcxw@gmail.com>
2021-04-06 22:31:17 +0100

    Pangrams are now colored in gold

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,6 +1,7 @@
 import Answer from './Answer'
 
 const Answers = ({ answers, currentAnswers, gameState }) => {
+    let pangram = answers.reduce((prev, current) => (prev.length > current.length) ? prev : current)
     return (
         <div className='w-full h-full md:h-72 bg-white rounded-3xl shadow-md flex'>
             <div className='grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 grid-rows-6 grid-flow-row gap-4 p-6 w-full'>
@@ -8,6 +9,7 @@ const Answers = ({ answers, currentAnswers, gameState }) => {
                     <Answer
                         text={answer}
                         isVisible={currentAnswers.includes(answer)}
+                        isPangram={answer === pangram}
                         gameState={gameState}
                         key={index}
                     />
