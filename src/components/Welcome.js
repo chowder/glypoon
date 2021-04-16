fd9cd0cd6f9d0310420473e0598b308ef4d03455
chowder <andrewcxw@gmail.com>
2021-04-16 10:57:56 +0100

    Add practice mode button
    Add rules to the welcome dialog

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,21 @@
 import { useStoreActions, useStoreState } from 'easy-peasy'
 import { useState, Fragment } from 'react'
-import { Dialog, Transition } from "@headlessui/react"
+import { Dialog, Transition, Disclosure } from "@headlessui/react"
+import { ChevronUpIcon } from "@heroicons/react/solid";
 import GameState from '../classes/GameState'
 
 const Welcome = () => {
     const gameDuration = useStoreState(store => store.secondsRemaining)
     const setGameState = useStoreActions(actions => actions.setGameState)
+    const fetchNewSolution = useStoreActions(actions => actions.fetchNewSolution)
     const [open, setOpen] = useState(true)
 
-    const handleClose = () => {
+    const handlePractice = () => {
+        setOpen(false)
+        fetchNewSolution()
+    }
+
+    const handlePlay = () => {
         setOpen(false)
         setGameState(GameState.RUNNING)
     }
@@ -21,7 +28,7 @@ const Welcome = () => {
                 className="fixed inset-0 z-10 overflow-y-auto"
                 static
                 open={open}
-                onClose={handleClose}
+                onClose={handlePractice}
             >
                 <div className="min-h-screen px-8 text-center">
                     <Transition.Child
@@ -58,12 +65,36 @@ const Welcome = () => {
                                     Create as many words as you can in {Math.floor(gameDuration / 60)} minutes!
                                 </p>
                             </div>
+                            <Disclosure>
+                                {({ open }) => (
+                                    <>
+                                        <Disclosure.Button className="mt-4 flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                            <span>Game rules</span>
+                                            <ChevronUpIcon className={`${open ? "transform rotate-180" : ""} w-5 h-5 text-purple-500`} />
+                                        </Disclosure.Button>
+                                        <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-left text-gray-500">
+                                            <ul className="list-disc px-4">
+                                                <li>Each letter may only be used once</li>
+                                                <li>Each word must use the center letter</li>
+                                                <li>The rules for word acceptability can be found <a href="https://github.com/chowder/glypoon/blob/main/RULES.md" target="_blank" className="underline text-blue-500">here</a></li>
+                                            </ul>
+                                        </Disclosure.Panel>
+                                    </>
+                                )}
+                            </Disclosure>
 
                             <div className="mt-5 flex flex-row space-x-4 items-center justify-center">
                                 <button
                                     type="button"
                                     className="w-full py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                                    onClick={handleClose}
+                                    onClick={handlePractice}
+                                >
+                                    Practice
+                                </button>
+                                <button
+                                    type="button"
+                                    className="w-full py-2 text-sm font-medium text-green-900 bg-green-100 border border-transparent rounded-md hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                                    onClick={handlePlay}
                                 >
                                     Play
                                 </button>
